841406a0bdc79e2f550d3f3f0c01ada5d41b1558
mattco98 <matthewcolsson@gmail.com>
2019-08-07 09:22:31 -0500

    Began work on Home page
    Added background image and logo with some parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { ParallaxProvider } from 'react-scroll-parallax';
 import HomePage from './pages/Home';
 import ModulesPage from './pages/Modules';
 import './App.scss';
 
 const App: React.FunctionComponent = () => {
   return (
-    <ParallaxProvider>
-      <Router>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/modules" component={ModulesPage} />
-      </Router>
-    </ParallaxProvider>
+    <Router>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/modules" component={ModulesPage} />
+    </Router>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import Parallax from 'react-springy-parallax';
+import FlexView from 'react-flexview';
+import logo from '../../assets/logo-long.png';
+import background from '../../assets/background.jpg';
+import './index.scss';
 
 export default () => {
   return (
-    <p>Home</p>
+    <Parallax pages={3}>
+      <Parallax.Layer
+        offset={0}
+        speed={1}
+        style={{ zIndex: 5 }}
+      >
+        <FlexView 
+          marginTop="35vh"
+          vAlignContent="center"
+          hAlignContent="center"
+        >
+          <img className="logo" src={logo} alt="Logo string" />
+        </FlexView>
+      </Parallax.Layer>
+      <Parallax.Layer
+        offset={0}
+        speed={0.5}
+        style={{ zIndex: 1 }}
+      >
+        <img className="background" src={background} alt="Background" />
+      </Parallax.Layer>
+    </Parallax>
   );
 };
