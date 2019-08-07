6e60d676e6cd18885da85e0667b771ed750db861
mattco98 <matthewcolsson@gmail.com>
2019-08-07 16:38:17 -0500

    Added Grommet, more home page work

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Grommet } from 'grommet';
 import HomePage from './pages/Home';
 import ModulesPage from './pages/Modules';
 import './App.scss';
 
 const App: React.FunctionComponent = () => {
   return (
-    <Router>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/modules" component={ModulesPage} />
-    </Router>
+    <Grommet>
+      <Router>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/modules" component={ModulesPage} />
+      </Router>
+    </Grommet>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Parallax from 'react-springy-parallax';
-import FlexView from 'react-flexview';
+import { Button, Box } from 'grommet';
 import logo from '../../assets/logo-long.png';
 import background from '../../assets/background.jpg';
 import './index.scss';
@@ -10,24 +10,49 @@ export default () => {
     <Parallax pages={3}>
       <Parallax.Layer
         offset={0}
-        speed={1}
+        speed={2}
         style={{ zIndex: 5 }}
       >
-        <FlexView 
-          marginTop="35vh"
-          vAlignContent="center"
-          hAlignContent="center"
+        <Box 
+          direction="row"
+          width="100%"
+          alignContent="center"
+          justify="center"
+          margin={{ top: '30vh' }}
         >
           <img className="logo" src={logo} alt="Logo string" />
-        </FlexView>
+        </Box>
       </Parallax.Layer>
       <Parallax.Layer
         offset={0}
-        speed={0.5}
+        speed={1.5}
+        style={{ zIndex: 5 }}
+      >
+        <Box 
+          direction="row" 
+          width="100%" 
+          height="6vh"
+          alignContent="center" 
+          justify="center"
+          margin={{ top: '50vh' }}
+        >
+          <Button
+            label="Downloads"
+            margin={{ right: '5vw' }}
+          />
+          <Button
+            label="Modules"
+          />
+        </Box>
+      </Parallax.Layer>
+      <Parallax.Layer
+        offset={0}
+        speed={1}
         style={{ zIndex: 1 }}
       >
         <img className="background" src={background} alt="Background" />
       </Parallax.Layer>
+
     </Parallax>
   );
 };
