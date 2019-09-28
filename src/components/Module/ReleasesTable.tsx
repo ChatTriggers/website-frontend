6c0c7029faadbd6d9f4007d4d57b54a7fc70ac68
Matthew Olsson <matthewcolsson@gmail.com>
2019-09-28 13:01:27 -0500

    Styled ReleasesTable
    Added chips for the version numbers

--- src/components/Module/ReleasesTable.tsx
+++ src/components/Module/ReleasesTable.tsx
@@ -4,6 +4,7 @@ import {
   List,
   ListItem,
   ListItemText,
+  Chip,
   Divider,
   Collapse,
   Theme,
@@ -14,8 +15,18 @@ import { IRelease } from '~types';
 import MarkdownRenderer from '~components/MarkdownRenderer';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  collapse: {
-    padding: theme.spacing(2, 2, 2, 4),
+  releaseTitle: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  releaseTypography: {
+    padding: theme.spacing(0, 1),
+  },
+  descDivider: {
+    margin: theme.spacing(0, 4, 1, 4),
+  },
+  body: {
+    padding: theme.spacing(0, 4),
   },
 }));
 
@@ -36,38 +47,46 @@ export default ({ releases }: IReleasesTableProps): JSX.Element => {
   };
 
   return (
-    <List
-      component="nav"
-    >
+    <List component="nav">
       <Divider />
-      {releases.sort((a, b) => b.createdAt - a.createdAt).map(release => (
-        <div key={release.id}>
-          <ListItem
-            button
-            onClick={onReleaseClick(release.id)}
-          >
-            <ListItemText primary={`v${release.releaseVersion} for ct v${release.modVersion}`} />
-            {openRelease === release.id ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          </ListItem>
-          <Collapse
-            in={openRelease === release.id}
-            timeout="auto"
-            unmountOnExit
-            className={classes.collapse}
-          >
-            <Typography>
-              Downloads:
-              {` ${release.downloads}`}
-            </Typography>
-            <br />
-            <Typography>
-              Changelog:
-            </Typography>
-            <MarkdownRenderer source={release.changelog} />
-          </Collapse>
-          <Divider />
-        </div>
-      ))}
+      {releases.sort((a, b) => b.createdAt - a.createdAt).map(release => {
+        const releaseChip = <Chip label={`v${release.releaseVersion}`} size="small" />;
+        const modChip = <Chip label={`v${release.modVersion}`} size="small" />;
+
+        const label = (
+          <div className={classes.releaseTitle}>
+            {releaseChip}
+            <Typography className={classes.releaseTypography}>for ct</Typography>
+            {modChip}
+          </div>
+        );
+
+        return (
+          <div key={release.id}>
+            <ListItem button onClick={onReleaseClick(release.id)}>
+              <ListItemText primary={label} />
+              {openRelease === release.id ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </ListItem>
+            <Collapse
+              in={openRelease === release.id}
+              timeout="auto"
+              unmountOnExit
+            >
+              <Divider className={classes.descDivider} />
+              <div className={classes.body}>
+                <Typography>
+                  Downloads:
+                  {` ${release.downloads}`}
+                </Typography>
+                <br />
+                <Typography>Changelog:</Typography>
+                <MarkdownRenderer source={release.changelog} />
+              </div>
+            </Collapse>
+            <Divider />
+          </div>
+        );
+      })}
     </List>
   );
 };
