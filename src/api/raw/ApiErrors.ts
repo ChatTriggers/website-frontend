8222a5d2d37ac447702f2a566b004c3cd775fbea
Matthew Olsson <matthewcolsson@gmail.com>
2019-09-28 00:46:18 -0500

    Remove old code

--- src/api/raw/ApiErrors.ts
+++ src/api/raw/ApiErrors.ts
@@ -75,39 +75,3 @@ export const validateStatusCode = <T>(
       throw new Error(`Received unexpected error status code from api endpoint: ${response.status}`);
   }
 };
-
-// type ApiErrorsFunc = {
-//   [K1 in keyof typeof ApiErrorsDesc]: {
-//     [K2 in keyof typeof ApiErrorsDesc[K1]]: (responseText: string) => {
-//       type: string;
-//       description: string;
-//       responseText: string;
-//     };
-//   };
-// } & {
-//   UNKNOWN_RESPONSE_CODE(responseCode: number): string;
-// };
-
-// export const ApiErrors = Object.keys(ApiErrorsDesc).reduce((prev, curr) => ({
-//   ...prev,
-//   [curr]: Object.entries(ApiErrorsDesc[curr as keyof typeof ApiErrorsDesc]).reduce((prev2, [key, desc]) => ({
-//     ...prev2,
-//     [key]: (responseText: string) => ({
-//       type: key,
-//       description: desc,
-//       responseText,
-//     }),
-//   }), {} as unknown as ApiErrorsFunc),
-// }), {
-//   UNKNOWN_RESPONSE_CODE: (statusCode: number) => `Received unrecognized status code from api endpoint: ${statusCode}`,
-// }) as unknown as ApiErrorsFunc;
-
-// export const validateStatusCode = <T>(response: AxiosResponse, validator: {
-//   [code: number]: () => T;
-// }): T => {
-//   if (!Object.keys(validator).includes(response.status.toString())) {
-//     throw ApiErrors.UNKNOWN_RESPONSE_CODE(response.status);
-//   }
-
-//   return validator[response.status]();
-// };
