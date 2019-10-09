a01b76ef3fa71d42f2cb58cc01245fe5234fc0d0
Matthew Olsson <matthewcolsson@gmail.com>
2019-10-09 14:54:57 -0500

    Got rid of page transitions
    :(

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,30 @@
 import React from 'react';
-import {
-  BrowserRouter as Router, Route, Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
-import posed, { PoseGroup } from 'react-pose';
 import Drawer from '~components/Drawer';
 import theme from './styles/theme';
 import routes from './routes';
-import {
-  modulesStore, globalStore, action, observer,
-} from '~store';
-
-interface IRouteContainerProps {
-  currLocation: string;
-  firstLoad: boolean;
-}
-
-const RouteContainer = posed.div(({ firstLoad, currLocation }: IRouteContainerProps) => {
-  if (firstLoad) {
-    setTimeout(action(() => { modulesStore.firstLoad = false; }), 500);
-  }
-
-  return {
-    enter: {
-      transform: 'translate(0%)',
-      transition: {
-        transform: { type: 'tween', ease: 'easeInOut', duration: firstLoad ? 0 : undefined },
-      },
-    },
-    exit: {
-      transform: `translate(${currLocation === '/modules' ? '-100' : '100'}%)`,
-      transition: {
-        transform: { type: 'tween', ease: 'easeInOut' },
-      },
-    },
-  };
-});
+import { globalStore, observer } from '~store';
 
 const App: React.FunctionComponent = observer(() => (
   <ThemeProvider theme={theme}>
     <Router>
-      <Route
-        render={({ location }) => (
-          <Drawer>
-            <PoseGroup>
-              <RouteContainer
-                key={location.pathname}
-                currLocation={location.pathname}
-                firstLoad={modulesStore.firstLoad}
-              >
-                <Switch location={location}>
-                  {routes.map(({ route, component, name }) => (
-                    <Route
-                      key={route}
-                      path={route}
-                      exact
-                      render={({ match }) => {
-                        globalStore.setDrawerTitle(name || match.params.module);
+      <Drawer>
+        <Switch>
+          {routes.map(({ route, component, name }) => (
+            <Route
+              key={route}
+              path={route}
+              exact
+              render={({ match }) => {
+                globalStore.setDrawerTitle(name || match.params.module);
 
-                        return component;
-                      }}
-                    />
-                  ))}
-                </Switch>
-              </RouteContainer>
-            </PoseGroup>
-          </Drawer>
-        )}
-      />
+                return component;
+              }}
+            />
+          ))}
+        </Switch>
+      </Drawer>
     </Router>
   </ThemeProvider>
 ));
